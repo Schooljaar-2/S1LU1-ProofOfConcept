8a46f99eaae0b1d1d81f298ee6e77aaa54f62689
SAAB.W <woutersaab@gmail.com>
2025-09-05 15:51:48 +0200

    ROW ROW ROW MY BOAT

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express", page: "" });
+});
+
+router.get("/frikandel", (req, res, next) => {
+  res.render("frikandel");
 });
 
 export default router;
