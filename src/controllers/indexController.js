6f3a6dcfed558210165e6ac64632aa07ec84866d
BaasW <wan.baas@student.avans.nl>
2025-09-11 12:28:09 +0200

    Standard Landing page generated.

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -3,6 +3,6 @@ import { getTop10Films } from "../database/dao/movies.js";
 export const index = (req, res, next) => {
   getTop10Films((error, response) => {
     if (error) return next(error);
-    res.render("index", { title: "Express", page: "", movies: response });
+    res.render("index", { title: "Express", hotMovies: response });
   });
 };
