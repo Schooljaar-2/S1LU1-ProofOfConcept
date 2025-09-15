c69641402fc7e7edb395029262314f9d730ffe55
SAAB.W <woutersaab@gmail.com>
2025-09-15 09:37:24 +0200

    if statement for database online or local with .env variables.

--- src/database/db.js
+++ src/database/db.js
@@ -3,12 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isDeployment = process.env.DEV_ENVIRONMENT === "deployment";
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: isDeployment ? process.env.DB_ONLINE_HOST : process.env.DB_HOST,
+  user: isDeployment ? process.env.DB_ONLINE_USER : process.env.DB_USER,
+  password: isDeployment
+    ? process.env.DB_ONLINE_PASSWORD
+    : process.env.DB_PASSWORD,
+  database: isDeployment ? process.env.DB_ONLINE_NAME : process.env.DB_NAME,
+  port: isDeployment ? process.env.DB_ONLINE_PORT : process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
